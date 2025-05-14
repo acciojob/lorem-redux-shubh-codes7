906e40d64433d2a7ed854209d63193fdf24c28a5
shubh-codes7 <shubhamcodes7@gmail.com>
2025-05-14 15:27:14 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,37 @@
 
 import React from "react";
 import './../styles/App.css';
+import {useDispatch, useSelector} from 'react-redux'
+import {fetchApi} from '../redux/loremSlice.js'
 
 const App = () => {
+  const dispatch = useDispatch()
+  const lorem = useSelector(state => state.lorem)
+  console.log(lorem)
+
+  React.useEffect(()=>{
+    dispatch(fetchApi())
+  }, [])
+
+  if(lorem.isLoading) return <h2>Loading...</h2>
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <h1 style={{textAlign: "center"}}>A short Narration of Lorem Ipsum</h1>
+        <p style={{textAlign: "center"}}>Below contains a title and body</p>
+        <div className="container">
+          {
+            lorem.data.length > 0 && lorem.data.map(text => {
+            return (
+            <div key={text.id} className="card">
+              <p><strong>Title</strong>:{text.title}</p> 
+              <p><strong>Body</strong>:{text.body}</p>
+            </div>
+          )
+          })
+          }
+        </div>
+
     </div>
   )
 }
---
+++ src/redux/loremSlice.js
@@ -0,0 +1,33 @@
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+
+export const fetchApi = createAsyncThunk("fetchApi", () => {
+    return fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(response => response.json())
+})
+
+const loremSlice = createSlice({
+    name: "lorem",
+    initialState: {
+        isLoading: false,
+        isError: false,
+        data: []
+    },
+    extraReducers: (builder) => {
+        builder.addCase(fetchApi.pending, (state, action) => {
+            state.isLoading = true
+        })
+
+        builder.addCase(fetchApi.fulfilled, (state, action) => {
+            state.isLoading = false
+            state.data = action.payload
+        })
+
+        builder.addCase(fetchApi.rejected, (state, action) => {
+            state.isLoading = false
+            state.isError = true
+        })
+    }
+})
+
+
+export default loremSlice.reducer
